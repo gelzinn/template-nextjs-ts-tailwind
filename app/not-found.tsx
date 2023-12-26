d672147c061316ae04027696f690dbec141e5349
gelzinn <60621494+gelzinn@users.noreply.github.com>
2023-12-26 13:51:05 -0300

    fix: change ` error.tsx` to ` not-found.tsx` page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1 className="text-6xl text-center">404</h1>
+
+      <p className="flex items-center justify-center">
+        This page does not exist.
+      </p>
+
+      <p className="flex items-center justify-center">
+        <a href="/" className="text-blue-600 hover:text-blue-800">
+          Go back to home
+        </a>
+      </p>
+    </>
+  );
+}
